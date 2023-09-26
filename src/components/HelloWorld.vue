6fb5002ab6a40d426db1ebe75ebd2f2e6fce3822
KE XUE <xueke.alan@gmail.com>
2023-09-26 17:17:58 +0800

    First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <button type="button" @click="count++">拉取</button>
+  <button type="button" @click="count++">查找</button>
+  <button type="button" @click="count++">删除</button>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
